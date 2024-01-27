288fe813474cdb74baf1ea247c24ec9bd9649d0e
ZinLinnhtet <mackk5504@gmail.com>
2024-01-27 14:32:57 +0630

    Add Jwt token

--- DotNet8WebApi/Program.cs
+++ DotNet8WebApi/Program.cs
@@ -1,11 +1,12 @@
 using DotNet8WebApi.AppDbContext;
-using Microsoft.AspNetCore.Http.Json;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using NUlid;
-using System.Text.Json;
-using System.Text.Json.Serialization;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+;
 
 // Add services to the container.
 
@@ -22,6 +23,23 @@ builder.Services.AddDbContext<AppDbContext>(opts =>
 ServiceLifetime.Transient,
 ServiceLifetime.Transient);
 
+var tokenOptions = builder.Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+var key = Encoding.ASCII.GetBytes(tokenOptions.Secret);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+       .AddJwtBearer(options =>
+       {
+           options.TokenValidationParameters = new TokenValidationParameters
+           {
+               ValidateIssuer = true,
+               ValidateAudience = true,
+               ValidateLifetime = true,
+               ValidateIssuerSigningKey = true,
+               ValidIssuer = tokenOptions.Issuer,
+               ValidAudience = tokenOptions.Audience,
+               IssuerSigningKey = new SymmetricSecurityKey(key),
+           };
+       });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
