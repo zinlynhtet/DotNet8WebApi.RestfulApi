9233fbd51dbd7c7b72df63b7489b712df27f3765
ZinLinnhtet <mackk5504@gmail.com>
2024-01-27 19:24:52 +0630

    Add JWT token

--- DotNet8WebApi/Models/TokenOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DotNet8WebApi.Models
-{
-    public class TokenOptions
-    {
-        public string Issuer { get; set; }
-        public string Audience { get; set; }
-        public string Secret { get; set; }
-    }
-}
