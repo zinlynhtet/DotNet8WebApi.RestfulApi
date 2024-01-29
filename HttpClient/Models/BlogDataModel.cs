63ae2bf7852521038e993355e6755e76a1e89a92
ZinLinnhtet <mackk5504@gmail.com>
2024-01-29 22:06:28 +0630

    [Complete] Httpclient read

---
+++ HttpClient/Models/BlogDataModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HttpClientExamples.Models
+{
+    [Table("Blog")]
+    public class BlogDataModel
+    {
+        [Key]
+        [Column("Blog_Id")]
+        public string Blog_Id { get; set; }
+        public string? Blog_Title { get; set; }
+        public string? Blog_Author { get; set; }
+        public string? Blog_Content { get; set; }
+
+    }
+    public class BlogListModel
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public List<BlogDataModel> Data { get; set; }
+
+
+    }
+}
