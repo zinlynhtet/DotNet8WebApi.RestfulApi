1f10aa51c6b2c97df4cebf84a914c5b9ff0b8e6f
ZinLinnhtet <mackk5504@gmail.com>
2024-01-30 00:28:17 +0630

    -

--- HttpClient/HttpClientExamples/HttpClientExample.cs
+++ HttpClient/HttpClientExamples/HttpClientExample.cs
@@ -3,8 +3,10 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace HttpClientExamples.HttpClientExample
 {
@@ -12,7 +14,9 @@ namespace HttpClientExamples.HttpClientExample
     {
         public async Task Run()
         {
-            await BlogGetList();
+            //await BlogGetList();
+            await BlogGetById("01HN3GNMVGDTKYB17EGFCPCBDG");
+            await BlogCreate("HttpClient", "HttpClient", "HttpClient");
         }
         public async Task BlogGetList()
         {
@@ -31,26 +35,47 @@ namespace HttpClientExamples.HttpClientExample
                 }
             }
 
-            //public async Task BlogGetbyId()
-            //{
+        }
+        public async Task BlogGetById(string id)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync($"https://localhost:7091/api/blog/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonStr = await response.Content.ReadAsStringAsync();
+                var model = JsonConvert.DeserializeObject<BlogDataResponseModel>(jsonStr);
+                var item = model.Data;
+                await Console.Out.WriteLineAsync(item.Blog_Id);
+                await Console.Out.WriteLineAsync(item.Blog_Title);
+                await Console.Out.WriteLineAsync(item.Blog_Author);
+                await Console.Out.WriteLineAsync(item.Blog_Content);
+            }
+            else
+            {
+                await Console.Out.WriteLineAsync("No data found.");
+            }
 
-            //}
-            //public async Task BlogCreate()
-            //{
-
-            //}
-            //public async Task BlogPut()
-            //{
-
-            //}
-            //public async Task BlogPatch()
-            //{
-
-            //}
-            //public async Task BlogDelete()
-            //{
-
-            //}
+        }
+        public async Task BlogCreate(string title, string author, string content)
+        {
+            BlogViewModel reqModel = new BlogViewModel
+            {
+                Blog_Title = title,
+                Blog_Author = author,
+                Blog_Content= content
+            };
+            string jsonBlog = JsonConvert.SerializeObject(reqModel);
+            HttpContent httpContent = new StringContent(jsonBlog, Encoding.UTF8, Application.Json);
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:7091/api/");
+            var response = await client.PostAsync("blog", httpContent);
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonStr = await response.Content.ReadAsStringAsync();
+                var model = JsonConvert.DeserializeObject<BlogDataResponseModel>(jsonStr);
+                await Console.Out.WriteLineAsync(model!.Message);
+            }
+            
         }
     }
 }
