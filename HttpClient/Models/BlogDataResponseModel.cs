3a4705b5e24e53f0fd7c539012e91e36a7c1124d
ZinLinnhtet <mackk5504@gmail.com>
2024-01-29 22:06:33 +0630

    [Complete] Httpclient read

---
+++ HttpClient/Models/BlogDataResponseModel.cs
@@ -0,0 +1,15 @@
+﻿namespace HttpClientExamples.Models
+{
+    public class BlogDataResponseModel
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public BlogDataModel Data { get; set; }
+    }
+    public class BlogResponseModel
+    {
+        public int PageNo { get; set; }
+        public int PageSize { get; set; }
+        public List<BlogDataModel> Data { get; set; }
+    }
+}
