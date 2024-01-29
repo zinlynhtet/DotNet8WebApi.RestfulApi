27da3490d1b6adcbd763f4b7650d6f6f120e63cb
ZinLinnhtet <mackk5504@gmail.com>
2024-01-30 00:28:24 +0630

    -

--- HttpClient/Models/BlogDataModel.cs
+++ HttpClient/Models/BlogDataModel.cs
@@ -20,6 +20,12 @@ namespace HttpClientExamples.Models
         public string Message { get; set; }
         public List<BlogDataModel> Data { get; set; }
 
+    }
+    public class BlogViewModel
+    {
+        public string? Blog_Title { get; set; }
+        public string? Blog_Author { get; set; }
+        public string? Blog_Content { get; set; }
 
     }
 }
