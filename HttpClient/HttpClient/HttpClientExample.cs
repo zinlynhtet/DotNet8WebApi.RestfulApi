bf27e3b5f98bef731b94cc8fd9f017fe19ff33b1
ZinLinnhtet <mackk5504@gmail.com>
2024-01-29 21:45:20 +0630

    Add HttpClient Methods

---
+++ HttpClient/HttpClient/HttpClientExample.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClient.HttpClientExample
+{
+    public class HttpClientExample
+    {
+        public async Task Run()
+        {
+
+        }
+        public async Task BlogGetList()
+        {
+
+        }
+        public async Task BlogGetbyId()
+        {
+
+        }
+        public async Task BlogCreate()
+        {
+
+        }
+        public async Task BlogPut()
+        {
+
+        }
+        public async Task BlogPatch()
+        {
+
+        }
+        public async Task BlogDelete()
+        {
+
+        }
+    }
+}
