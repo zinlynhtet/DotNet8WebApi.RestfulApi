0a7c0b540c47334a4d233b86b9cc73d0ae5ebc5d
ZinLinnhtet <mackk5504@gmail.com>
2024-01-31 21:25:26 +0630

    [Add] Bogus

---
+++ DotNet8WebApi/Features/Blogs/BlogFakerExample.cs
@@ -0,0 +1,15 @@
+﻿using Bogus;
+using DotNet8WebApi.Models;
+
+namespace DotNet8WebApi.Features.Blogs
+{
+    public class BlogFakerExample  : Faker<BlogFakerModel>
+    {
+        public BlogFakerExample()
+        {
+            RuleFor(o => o.Blog_Title, f => f.Lorem.Sentence(10));
+            RuleFor(o => o.Blog_Author, f => f.Lorem.Word());
+            RuleFor(o => o.Blog_Content, f => f.Lorem.Word());
+        }
+    }
+}
