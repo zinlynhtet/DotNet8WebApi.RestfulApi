e27fe1a76c5d81938bb4020977f66a5e9c834740
ZinLinnhtet <mackk5504@gmail.com>
2024-01-31 21:25:30 +0630

    [Add] Bogus

---
+++ DotNet8WebApi/Features/Blogs/BlogFakerModel.cs
@@ -0,0 +1,19 @@
+﻿using NUlid;
+
+namespace DotNet8WebApi.Features.Blogs
+{
+    public class BlogFakerModel
+    {
+        public string? Blog_Id { get; set; } = Ulid.NewUlid().ToString();
+        public string? Blog_Title { get; set; }
+        public string? Blog_Author { get; set; }
+        public string? Blog_Content { get; set; }
+    }
+    public class BlogFakerListModel
+    {
+        public string? Blog_Id { get; set; } = Ulid.NewUlid().ToString();
+        public string? Blog_Title { get; set; }
+        public string? Blog_Author { get; set; }
+        public string? Blog_Content { get; set; }
+    }
+}
