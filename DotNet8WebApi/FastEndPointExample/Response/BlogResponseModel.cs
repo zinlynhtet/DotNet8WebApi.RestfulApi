667fbefc20dfa15496883547d8acbc9de9903568
ZinLinnhtet <mackk5504@gmail.com>
2024-02-04 00:12:22 +0630

    -

---
+++ DotNet8WebApi/FastEndPointExample/Response/BlogResponseModel.cs
@@ -0,0 +1,14 @@
+﻿namespace DotNet8WebApi.FastEndpointExample.RequestAndResponse.Response
+{
+    public class BlogResponseModel
+    {
+        public string BlogData { get; set; }
+        public string BlogAuthor { get; set; }
+    }
+    public class MyResponse
+    {
+        public string? Blog_Title { get; set; }
+        public string? Blog_Author { get; set; }
+        public string? Blog_Content { get; set; }
+    }
+}
