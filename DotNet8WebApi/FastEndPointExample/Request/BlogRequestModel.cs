213c0be1f8a1c4eff5def5aeb917f429444ee3b7
ZinLinnhtet <mackk5504@gmail.com>
2024-02-04 00:12:19 +0630

    -

---
+++ DotNet8WebApi/FastEndPointExample/Request/BlogRequestModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNet8WebApi.FastEndpointExample.RequestAndResponse.Request
+{
+    public class BlogRequestModel
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string BlogTitle { get; set; }
+        public string BlogAuthor { get; set; }
+        public string BlogContent { get; set; }
+    }
+}
