a24473133e78ede8ef333a90b0acb1e3990610a9
ZinLinnhtet <mackk5504@gmail.com>
2024-02-04 00:12:16 +0630

    -

---
+++ DotNet8WebApi/FastEndPointExample/FastEndPointController/BlogPostController.cs
@@ -0,0 +1,25 @@
+﻿using DotNet8WebApi.FastEndpointExample.RequestAndResponse.Request;
+using DotNet8WebApi.FastEndpointExample.RequestAndResponse.Response;
+using FastEndpoints;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet8WebApi.FastEndpointExample.BlogPostControllers
+{
+    public class MyEndpoint : Endpoint<BlogRequestModel, BlogResponseModel>
+    {
+        public override void Configure()
+        {
+            Post("/api/user/create");
+            AllowAnonymous();
+        }
+        public override async Task HandleAsync(BlogRequestModel reqModel, CancellationToken ct)
+        {
+            await SendAsync(new()
+            {
+                BlogData = reqModel.BlogTitle + "," + reqModel.BlogContent,
+                BlogAuthor = reqModel.BlogAuthor,
+            });
+        }
+    }
+
+}
