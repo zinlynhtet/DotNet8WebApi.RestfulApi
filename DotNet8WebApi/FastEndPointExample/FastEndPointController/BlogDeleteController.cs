42a47f558481f58dc18af8e6f1029a3e431dea0b
ZinLinnhtet <mackk5504@gmail.com>
2024-02-05 22:35:38 +0630

    -

---
+++ DotNet8WebApi/FastEndPointExample/FastEndPointController/BlogDeleteController.cs
@@ -0,0 +1,34 @@
+﻿using DotNet8WebApi.EFDbContext;
+using DotNet8WebApi.FastEndpointExample.RequestAndResponse.Request;
+using DotNet8WebApi.FastEndpointExample.RequestAndResponse.Response;
+using FastEndpoints;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace DotNet8WebApi.FastEndpointExample.BlogDeleteController
+{
+    public class MyEndpoint : Endpoint<BlogIdRequestModel,BlogResponseModel>
+    {
+        private readonly AppDbContext _context;
+
+        public MyEndpoint(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public override void Configure()
+        {
+            Delete("/api/blog/{Id}");
+            AllowAnonymous();
+        }
+
+        public override async Task HandleAsync(BlogIdRequestModel reqId, CancellationToken ct)
+        {
+            var item = await _context.Data.FindAsync(reqId);
+            // _context.Remove(item);
+            await SendAsync(new BlogResponseModel()
+            {
+               IsSuccess = item != null,
+            });
+        }
+    }
+}
